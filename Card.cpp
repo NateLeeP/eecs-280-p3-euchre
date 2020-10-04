1ff315ceb078ce731bdc3089a9e397338220f81c
nlpruitt <nlpruitt@gmail.com>
2020-10-03 21:34:57 -0400

    Initial euchre commit

---
+++ Card.cpp
@@ -0,0 +1,48 @@
+// Project UID 1d9f47bfc76643019cfbf037641defe1
+
+#include <cassert>
+#include <iostream>
+#include <string>
+#include "Card.h"
+// add any necessary #include or using directives here
+
+// rank and suit names -- do not remove these
+constexpr const char* const Card::RANK_TWO;
+constexpr const char* const Card::RANK_THREE;
+constexpr const char* const Card::RANK_FOUR;
+constexpr const char* const Card::RANK_FIVE;
+constexpr const char* const Card::RANK_SIX;
+constexpr const char* const Card::RANK_SEVEN;
+constexpr const char* const Card::RANK_EIGHT;
+constexpr const char* const Card::RANK_NINE;
+constexpr const char* const Card::RANK_TEN;
+constexpr const char* const Card::RANK_JACK;
+constexpr const char* const Card::RANK_QUEEN;
+constexpr const char* const Card::RANK_KING;
+constexpr const char* const Card::RANK_ACE;
+
+constexpr const char* const Card::SUIT_SPADES;
+constexpr const char* const Card::SUIT_HEARTS;
+constexpr const char* const Card::SUIT_CLUBS;
+constexpr const char* const Card::SUIT_DIAMONDS;
+
+// add your code below
+
+Card::Card() {
+	rank = Card::RANK_TWO;
+	suit = Card::SUIT_SPADES;
+	
+}
+
+Card::Card(const std::string& rank_in, const std::string& suit_in) {
+	rank = rank_in;
+	suit = suit_in;
+}
+
+std::string Card::get_rank() const {
+	return rank;
+}
+
+std::string Card::get_suit() const {
+	return suit;
+}
